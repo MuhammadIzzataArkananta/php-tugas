c20d983cafec45c44a5d50c0369e9ab99987c167
MuhammadIzzataArkananta <bankfree426@gmail.com>
2024-02-18 23:45:07 +0700

    update data
    video #22

---
+++ youtube-php/restoran/kategori/update.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "../function.php";
+
+$sql="SELECT * FROM tblkategori WHERE idkategori=$id";
+$result=mysqli_query($koneksi, $sql);
+$row=mysqli_fetch_assoc($result);
+
+// $kategori='es jus';
+// $id=20;
+// $sql="UPDATE tblkategori SET kategori='$kategori' WHERE idkategori=$id";
+// $result=mysqli_query($koneksi, $sql);
+// echo $sql;
+?>
+
+<form action="" method="post">
+    kategori :
+    <input type="text" name="kategori" value="<?php echo $row['kategori']?>">
+    <br>
+    <input type="submit" name="simpan" value="simpan">
+</form>
+<?php 
+
+if(isset($_POST['simpan'])) {
+    $kategori=$_POST['kategori'];
+    $sql="UPDATE tblkategori SET kategori='$kategori' WHERE idkategori=$id;";
+    $result=mysqli_query($koneksi, $sql);
+    
+    header("location:http://localhost/php-tugas/php-tugas/youtube-php/restoran/kategori/select.php");
+}
+?>
