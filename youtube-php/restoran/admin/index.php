1d3c0e2c6f0910b6ddf421e99663364fb820015b
MuhammadIzzataArkananta <bankfree426@gmail.com>
2024-02-19 17:25:14 +0700

    pembuatan layout aplikasi restoran berbasis web
    video #25

---
+++ youtube-php/restoran/admin/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page | Aplikasi Restoran SMK</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h2>Restoran</h2>
+            </div>
+            <div class="col-md-9">
+                <div class="float-end mt-3">logout</div>  
+            </div>
+        </div>
+      
+        <div class="row">
+            <div class="col-md-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item"><a class="nav-link" href="#">Kategori</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Pelanggan</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Order</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Order Detail</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Order Detail</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">User</a></li> 
+                </ul>
+            </div>
+            <div class="col-md-9">
+
+            </div>
+        </div>
+        
+        <div class="row mt-5">
+            <div class="col">
+                <p class="text-center">2024 - copyright@arkablaze.com</p>
+            </div>     
+        </div>
+    </div>
+</body>
+</html>
