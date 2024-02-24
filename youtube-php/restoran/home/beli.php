edc1922fb2d5b434bde2115016f8390516ffc24a
MuhammadIzzataArkananta <bankfree426@gmail.com>
2024-02-24 16:11:17 +0700

    Menambah Dan Menghapus Isi Cart Aplikasi Restoran Berbasis Web
    video #45

--- youtube-php/restoran/home/beli.php
+++ youtube-php/restoran/home/beli.php
@@ -7,6 +7,20 @@ if(isset($_GET['hapus'])) {
     unset($_SESSION['_'.$id]);
 }
 
+if(isset($_GET['tambah'])) {
+    $id=$_GET['tambah'];
+    $_SESSION['_'.$id]++;
+}
+
+if(isset($_GET['kurang'])) {
+    $id=$_GET['kurang'];
+    $_SESSION['_'.$id]--;
+
+    if($_SESSION['_'.$id]==0) {
+        unset($_SESSION['_'.$id]);
+        }
+}
+
 if(!isset($_SESSION['pelanggan'])) {
     header("location:?f=home&m=login");
 }
@@ -36,8 +50,10 @@ function keranjang() {
     
     global $db;
 
+    $total=0;
+
     echo '
-    <table class="table table-bordered w-50">
+    <table class="table table-bordered w-70">
         <tr>
             <th>Menu</th>
             <th>Harga</th>
@@ -57,13 +73,20 @@ function keranjang() {
                 echo '<tr>';
                 echo '<td>'.$r['menu'].'</td>';
                 echo '<td>'.$r['harga'].'</td>';
-                echo '<td>'.$value.'</td>';
+                echo '<td><a href="?f=home&m=beli&tambah='.$r['idmenu'].'">[+]</a> &nbsp &nbsp'.$value.'&nbsp &nbsp <a href="?f=home&m=beli&kurang='.$r['idmenu'].'">[-]</a></td>';
                 echo '<td>'.$r['harga'] * $value.'</td>';
                 echo '<td><a href="?f=home&m=beli&hapus='.$r['idmenu'].'">Hapus</a></td>';
                 echo '</tr>';
+                $total=$total + ($value * $r['harga']);
             }
         }   
     }
+
+    echo '<tr>
+        <td colspan=4><h3>Grand Total :</h3></td>
+        <td><h3>'.$total.'</h3></td>
+    
+    <tr>';
 
     echo '</table>';
 }
