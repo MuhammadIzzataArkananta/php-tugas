1e16257cce8adf08d153e2e0d6775f552773a8f4
MuhammadIzzataArkananta <bankfree426@gmail.com>
2024-02-24 21:06:07 +0700

    Menambahkan Jumlah Pembelian Aplikasi Restoran Berbasis Web
    video #49

--- youtube-php/restoran/index.php
+++ youtube-php/restoran/index.php
@@ -11,6 +11,26 @@ if(isset($_GET['log'])) {
     header("location:index.php");
 }
 
+function cart() {
+    global $db;
+
+    $cart=0;
+
+    foreach($_SESSION as $key =>$value) {
+        if($key<>'pelanggan' && $key<>'idpelanggan') {
+            $id=substr($key,1); 
+            $sql="SELECT * FROM tblmenu WHERE idmenu=$id";
+            $row=$db->getALL($sql);
+
+            foreach($row as $r) {
+                $cart++;
+            }
+        }
+    }
+
+    return $cart;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +53,8 @@ if(isset($_GET['log'])) {
                     if(isset($_SESSION['pelanggan'])) {
                         echo ' 
                             <div class="float-end mt-3"><a href="?log=logout">Logout</a></div>
-                            <div class="float-end mt-3 me-3">Pelanggan : <a href="?f=home&m=beli"> '.$_SESSION['pelanggan'].'</a></div>  
+                            <div class="float-end mt-3 me-3">Pelanggan : '.$_SESSION['pelanggan'].' </div>
+                            <div class="float-end mt-3 me-3">Cart : ( <a href="?f=home&m=beli"> '.cart().'</a> ) </div>  
                         ';
                     }
                     else {
